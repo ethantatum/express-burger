0c4e5df06a62b1223aab0a99b59ce0df2575f59f
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-02-15 09:26:50 -0700

    mysql files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Habanero Relleno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Grilled Turkey and Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Steak Tartare Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean and Fennel Veggie Burger')
