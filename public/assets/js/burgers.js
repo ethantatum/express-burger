e1af156266f054c80d75aa2a1df17355a973ef5d
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-02-20 11:31:32 -0700

    added burgers.js file

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,40 @@
+$(function() {
+    $('.change-eaten').on('click', function(event) {
+        let id = $(this).data('id');
+        let eaten = $(this).data('eaten');
+
+        let eatenState = {
+            devoured: eaten
+        };
+
+        // Send the PUT request
+        $.ajax(`/api/burgers/${id}`, {
+            type: 'PUT',
+            data: eatenState
+        }).then(
+            function() {
+                console.log(`Changed devoured to ${eaten}`);
+                location.reload();
+            }
+        );
+    });
+
+    $('.create-form').on('submit', function(event) {
+        event.preventDefault();
+
+        let newBurger = {
+            burger_name: $('#brgr').val().trim(),
+        };
+
+        // Send the POST request
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: newBurger
+        }).then(
+            function() {
+                console.log('Created a new burger');
+                location.reload();
+            }
+        );
+    });
+});
